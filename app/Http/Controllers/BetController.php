3484753e246164b41952f7b5038305f91da4f5d5
Kevin Roleke <kevin@zerogon.consulting>
2025-03-06 18:58:42 -0500

    Fix cash out multiplier

--- app/Http/Controllers/BetController.php
+++ app/Http/Controllers/BetController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use App\Models\Bet;
 use Illuminate\Http\Request;
 use App\Events\UpdateBet;
+use App\Models\Game;
+use Illuminate\Support\Facades\Log;
 
 class BetController extends Controller
 {
@@ -20,7 +22,7 @@ class BetController extends Controller
             'done' => false,
         ]);
 
-        broadcast(new UpdateBet($bet));
+        broadcast(new UpdateBet($bet->load('user')));
 
         return ['placed' => true];
     }
@@ -30,16 +32,18 @@ class BetController extends Controller
      */
     public function update(Request $request)
     {
-        $bet = Bet::with('game')->where('user_id', auth()->user()->id)->where('done', false)->firstOrFail();
-        if ($bet->$game->end_time <= now() || $bet->$game->done) {
+        $bet = Bet::with('game')->where('user_id', auth()->user()->id)->where('bet_deadline', '<', now())->where('done', false)->firstOrFail();
+        if ($bet->game->end_time <= now() || $bet->game->done) {
             return ['cashed' => false];
         }
 
-        $msAfterStart = now()->diffWithCarbonInterval($bet->$game->bet_deadline)->total('milliseconds');
+        $msAfterStart = $bet->game->bet_deadline->diffInMilliseconds(now());
+        Log::info($msAfterStart);
         $bet->multiplier = 100 + $msAfterStart/100;
         $bet->done = true;
 
         $bet->save();
-        broadcast(new UpdateBet($bet));
+        broadcast(new UpdateBet($bet->load('user')));
+        return ['cashed' => true];
     }
 }
