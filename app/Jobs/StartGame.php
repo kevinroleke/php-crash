07649b0737cfd784afa47559ab9d0538899e565d
Kevin Roleke <kevin@zerogon.consulting>
2025-03-05 23:41:18 -0500

    begin game job, multiplier RNG 1% house edge

---
+++ app/Jobs/StartGame.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Jobs;
+
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Queue\Queueable;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use App\Models\Game;
+
+class StartGame implements ShouldQueue
+{
+    use Dispatchable, Queueable, SerializesModels, InteractsWithQueue;
+
+    /**
+     * Create a new job instance.
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Execute the job.
+     */
+    public function handle(): void
+    {
+        if (Game::where('done', false)->count()) {
+            // active game already
+            return;
+        }
+
+        $n = gmp_random_range(0, 4503599627370495);
+        $n /= 2**52;
+        $n = max(floor(99 / (1 - n)), 100);
+
+        $game = new Game;
+        $game->start_time = now();
+        $game->bet_deadline = now()->addSeconds(15);
+        $game->done = false;
+        $game->multiplier = $n;
+        $game->end_time = now()->addSeconds(15)->addMilliseconds($n*100);
+        $game->save();
+    }
+}
