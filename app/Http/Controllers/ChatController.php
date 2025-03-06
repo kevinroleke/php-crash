ea97b3ff4c32b91892685de3134575dd4c3234f5
Kevin Roleke <kevin@zerogon.consulting>
2025-03-06 12:07:26 -0500

    chat controller

---
+++ app/Http/Controllers/ChatController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Chat;
+use Illuminate\Http\Request;
+use App\Events\ChatMessage;
+
+class ChatController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return Chat::with('user')->orderByDesc('done')->limit(1)->get();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $chat = auth()->user()->chats()->create([
+            'message' => $request->message,
+        ]);
+
+        broadcast(new ChatMessage($chat->load('user')))->toOthers();
+
+        return ['sent' => true];
+    }
+}
