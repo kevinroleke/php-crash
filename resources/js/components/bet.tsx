a764389f08e153c93bc26024e4c06d01028b70bc
Kevin Roleke <kevin@zerogon.consulting>
2025-03-06 19:24:33 -0500

    disable bet & cash buttons when not available

--- resources/js/components/bet.tsx
+++ resources/js/components/bet.tsx
@@ -1,24 +1,53 @@
 import { Input } from '@/components/ui/input';
 import { Button } from '@/components/ui/button';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import axios from 'axios';
+import Echo from 'laravel-echo';
+import Pusher from 'pusher-js';
 
 export function Bet() {
-    const [amount, setAmount] = useState(0);
+    const [cashDisabled, setCashDisabled] = useState(true);
+    const [betDisabled, setBetDisabled] = useState(true);
+    useEffect(() => {
+        //@ts-ignore
+        window.Pusher = Pusher;
+        let e = new Echo({
+            broadcaster: 'reverb',
+            key: import.meta.env.VITE_REVERB_APP_KEY,
+            wsHost: import.meta.env.VITE_REVERB_HOST,
+            wsPort: import.meta.env.VITE_REVERB_PORT ?? 80,
+            wssPort: import.meta.env.VITE_REVERB_PORT ?? 443,
+            forceTLS: (import.meta.env.VITE_REVERB_SCHEME ?? 'https') === 'https',
+            enabledTransports: ['ws', 'wss'],
+        });
+        e.channel('game').listen('.NewGame', (c: any) => {
+            setBetDisabled(false);
+        });
+        e.channel('game').listen('.GameEnd', (c: any) => {
+            setCashDisabled(true);
+        });
+        e.channel('game').listen('.BetsClosed', (c: any) => {
+            setBetDisabled(true);
+            setCashDisabled(false);
+        });
+    }, []);
+    const [amount, setAmount] = useState();
 
     const place = () => {
+        setBetDisabled(true);
         axios.put(route('bet'), {amount})
     };
 
     const cash = () => {
+        setCashDisabled(true);
         axios.post(route('bet'))
     };
 
     return (
         <div className="flex flex-col p-5 rounded-xl h-full">
             <Input min="1" type="number" placeholder="Amount" value={amount} onChange={e => setAmount(parseFloat(e.target.value))} />
-            <Button className="mt-1" type='submit' onClick={place} variant={'secondary'}>Place Bet</Button>
-            <Button className="mt-1" variant={'secondary'} onClick={cash}>Cashout</Button>
+            <Button className="mt-1" disabled={betDisabled} type='submit' onClick={place} variant={'secondary'}>Place Bet</Button>
+            <Button className="mt-1" disabled={cashDisabled} variant={'secondary'} onClick={cash}>Cashout</Button>
         </div>
     );
 }
