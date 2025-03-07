bac6da8926daf0b650d256091c1185f63cc6d5f3
Kevin Roleke <kevin@zerogon.consulting>
2025-03-06 20:35:46 -0500

    show game on the homepage

--- routes/game.php
+++ routes/game.php
@@ -4,6 +4,4 @@ use App\Http\Controllers\GameController;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
-Route::middleware('auth')->group(function () {
-    Route::get('game', [GameController::class, 'index'])->name('game');
-});
+Route::get('game', [GameController::class, 'index'])->name('game');
